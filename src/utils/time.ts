57ad5e05b52295c178253d7a5ab3d6dff35078d1
Juan I Casareski <juan@snabb.cl>
2023-10-06 10:51:24 -0300

    feat: update time after changing channel

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -7,11 +7,9 @@ dayjs.extend(duration)
 export const get_epoch_time = () => new Date().getTime();
 
 export const get_time_since_action = (time_of: number, old_action: UserActions, new_action: UserActions): number => {
-    if (old_action == 'connected' && new_action == 'disconnected') {
-        return get_epoch_time() - time_of
-    }    
+    if ([old_action, new_action].includes("connected")) 
+        return get_epoch_time() - time_of;
 
-    // Devuelve 0 si se conectó o si las acciones son las mismas.
     return 0;
 }
 
